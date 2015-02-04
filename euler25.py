90c2d2f1f2517788629815d0ebefecc5f4632e2e
euporie <random8902@gmail.com>
2015-02-04 13:44:18 +0100

    Easy solution to problem 5

--- euler25.py
+++ euler25.py
@@ -1,10 +1,12 @@
+n=1000
 a=1
 b=1
 suma=0
 term = 0
-while(len(str(a))<=1000):
+while(len(str(a))<=n):
     term = term + 2
     print "Term: %s Equals: %s and has %s digits" % (term-1, a, len(str(a)))
     print "Term: %s Equals: %s and has %s digits" % (term, b, len(str(a)))
     a=a+b
     b=a+b
+    bar.update()
--- euler5.py
+++ euler5.py
@@ -1,10 +1,17 @@
-def gcd(a,b):
-    while b:
-        a,b = b, a%b
-    return a
+from math import factorial
+def is_divisible_by_20(n):
+    d=True
+    for i in range(1,20):
+        if n%i!=0:
+            d=False
+    return d
 
-def lcm(a,b):
-    return a*b
 
-def lcmm(*args):
-    return reduce(lcm,args)
+largest = factorial(20)
+current = largest
+
+for i in range(1,20):
+    if current%i==0 and is_divisible_by_20(current/i):
+        current=current/i
+        print current
+
