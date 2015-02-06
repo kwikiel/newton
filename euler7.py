100aff0ea31fff2ce001503ec435a32730d88acf
euporie <random8902@gmail.com>
2015-02-06 18:09:24 +0100

    Test prime checker

--- euler7.py
+++ euler7.py
@@ -1,12 +1,14 @@
 from math import ceil
+import unittest
 '''
 Eratostenes sieve
 '''
 def prime_test(n):
     if n==2 or n==3 or n==5:
         return True
-    for i in range(1,int(ceil(n**0.5))):
-        return False
+    for i in range(2,int(n/2)):
+        if n%i==0:
+            return True
     return True
 
 for i in range(1,1000):
@@ -14,4 +16,11 @@ for i in range(1,1000):
         print i
 
 
+class MathChecker(unittest.TestCase):
 
+    def test_small_primes(self):
+        self.assertTrue(prime_test(2))
+        self.assertFalse(prime_test(4))
+
+if __name__ == '__main__':
+    unittest.main()
